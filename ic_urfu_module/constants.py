df63edb5657842612c896f1721744729decefc5c
tkach_nikita <TkachLermontov@yandex.ru>
2026-03-29 13:36:33 +0500

    Linter and extra  stuff

--- ic_urfu_module/constants.py
+++ ic_urfu_module/constants.py
@@ -5,37 +5,32 @@ Centralizing constants makes it easier to maintain and modify default values.
 """
 
 # Document Generation
-DEFAULT_FONT_NAME = 'Times New Roman'
+DEFAULT_FONT_NAME = "Times New Roman"
 DEFAULT_FONT_SIZE = 12
-DEFAULT_DOCUMENT_FILENAME_PREFIX = 'Individual_Plan'
+DEFAULT_DOCUMENT_FILENAME_PREFIX = "Individual_Plan"
 
 # Document Header Text
-DOCUMENT_HEADER_LINE1 = 'МИНИСТЕРСТВО НАУКИ И ВЫСШЕГО ОБРАЗОВАНИЯ РОССИЙСКОЙ ФЕДЕРАЦИИ'
-DOCUMENT_HEADER_LINE2 = (
-    'Федеральное государственное автономное образовательное учреждение '
-    'высшего образования'
-)
-DOCUMENT_HEADER_LINE3 = (
-    '«Уральский федеральный университет имени первого Президента России Б.Н.Ельцина»'
-)
-DOCUMENT_TITLE = 'ИНДИВИДУАЛЬНЫЙ УЧЕБНЫЙ ПЛАН МАГИСТРАНТА'
+DOCUMENT_HEADER_LINE1 = "МИНИСТЕРСТВО НАУКИ И ВЫСШЕГО ОБРАЗОВАНИЯ РОССИЙСКОЙ ФЕДЕРАЦИИ"
+DOCUMENT_HEADER_LINE2 = "Федеральное государственное автономное образовательное учреждение высшего образования"
+DOCUMENT_HEADER_LINE3 = "«Уральский федеральный университет имени первого Президента России Б.Н.Ельцина»"
+DOCUMENT_TITLE = "ИНДИВИДУАЛЬНЫЙ УЧЕБНЫЙ ПЛАН МАГИСТРАНТА"
 
 # Section Labels
-SECTION_GENERAL_INFO = 'Общие сведения'
-SECTION_EDUCATION_PART = 'Образовательная часть программы подготовки'
-SECTION_MANDATORY = 'Обязательная часть в соответствии с учебным планом'
-SECTION_ELECTIVE = 'Дисциплины и курсы по выбору и факультативы'
-SECTION_CONCLUSION = 'Заключение кафедры:'
+SECTION_GENERAL_INFO = "Общие сведения"
+SECTION_EDUCATION_PART = "Образовательная часть программы подготовки"
+SECTION_MANDATORY = "Обязательная часть в соответствии с учебным планом"
+SECTION_ELECTIVE = "Дисциплины и курсы по выбору и факультативы"
+SECTION_CONCLUSION = "Заключение кафедры:"
 
 # Field Labels
-LABEL_INSTITUTE = 'Институт'
-LABEL_DEPARTMENT = 'Кафедра/Департамент'
-LABEL_SPECIALTY = 'Направление подготовки'
-LABEL_PROGRAM = 'Образовательная программа'
-LABEL_SUPERVISOR = 'Научный руководитель'
-LABEL_RESEARCH_AREA = 'Направление научно-исследовательской деятельности'
-LABEL_THESIS = 'Тема выпускной квалификационной работы'
-LABEL_DEADLINE = 'Срок предоставления ВКР к защите'
+LABEL_INSTITUTE = "Институт"
+LABEL_DEPARTMENT = "Кафедра/Департамент"
+LABEL_SPECIALTY = "Направление подготовки"
+LABEL_PROGRAM = "Образовательная программа"
+LABEL_SUPERVISOR = "Научный руководитель"
+LABEL_RESEARCH_AREA = "Направление научно-исследовательской деятельности"
+LABEL_THESIS = "Тема выпускной квалификационной работы"
+LABEL_DEADLINE = "Срок предоставления ВКР к защите"
 
 # Table Headers
 TABLE_HEADERS_SUBJECTS = [
@@ -44,14 +39,14 @@ TABLE_HEADERS_SUBJECTS = [
     "Объем аудит. работы, час",
     "Объем (зет)",
     "Форма аттестации",
-    "Оценка"
+    "Оценка",
 ]
 
 # Control Forms Mapping
 CONTROL_FORM_MAPPING = {
-    'exam': 'Экзамен',
-    'credit': 'Зачет',
-    'credit_grade': 'Зачет с оценкой',
+    "exam": "Экзамен",
+    "credit": "Зачет",
+    "credit_grade": "Зачет с оценкой",
 }
 
 # Validation Limits
@@ -65,10 +60,10 @@ MAX_CREDITS = 10
 # Default Values
 DEFAULT_HOURS = 34
 DEFAULT_CREDITS = 3
-DEFAULT_CONTROL_FORM = 'credit'
-DEFAULT_ACADEMIC_YEAR = '2025 / 2026'
-DEFAULT_YEAR = '2025'
-DEFAULT_DEADLINE = 'Май 2027'
+DEFAULT_CONTROL_FORM = "credit"
+DEFAULT_ACADEMIC_YEAR = "2025 / 2026"
+DEFAULT_YEAR = "2025"
+DEFAULT_DEADLINE = "Май 2027"
 
 # Document Formatting
 SEPARATOR_LENGTH_SHORT = 40
